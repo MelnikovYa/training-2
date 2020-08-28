50a315758e858c555697acbc764335af7cf9a302
Yaroslav <60092939+MelnikovYa@users.noreply.github.com>
2020-08-28 14:21:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-$('.owl-carousel').owlCarousel({
+$('.slider-wrapper').owlCarousel({
     margin:10,
     loop:true,
     items:1
+})
+
+$('.categories').owlCarousel({
+    loop:true,
+    nav: false,
+    items: 1,
 })
